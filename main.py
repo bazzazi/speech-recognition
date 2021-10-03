2120a740f29d2159f526ee2ecacac37a0e0dec42
Mohammad Ali Bazzazi <81759694+bazzazi@users.noreply.github.com>
2021-10-03 17:01:28 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+###############          ##########        #######   #######        #########      #######       #
+#              #        #          #             #         #       #         #           #
+#               #       #          #            #         #        #         #          #        #
+#              #        #          #           #         #         #         #         #         #
+###############         ############          #         #          ###########        #          #
+#              #        #          #         #         #           #         #       #           #
+#               #       #          #        #         #            #         #      #            #
+#              #        #          #       #         #             #         #     #             #
+###############         #          #      #######    #######       #         #    #######        #
+
+
+
+# Auto post Bot for instagram
+# Developer: Mohammad Ali Bazzazi (me)
+# by only drag and drop, post an image or video in your inta account Easily
+# Don't forget to follow me
+# enjoy  :)
+
+########################### START ###########################
+
+from os import system
+
+# installing requirements
+# note: connect to internet
+system("pip install speechrecognition")
+system("pip install pipwin")
+system("pipwin install pyaudio")
+
+
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("\n########################")
+    print("speak anything...")
+    r.adjust_for_ambient_noise(source)
+    voice = r.listen(source)
+    try:
+        text = r.recognize_google(voice)
+        print(f"you said => {text}")
+    except:
+        print("ops! something went wrong!")
+    print("########################")
+########################### END ###########################
